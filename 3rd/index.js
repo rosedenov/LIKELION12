1e74bde14d93991b719b21f005e5b811b721fdc6
tlsrose <rose.de.november@gmail.com>
2024-03-22 13:35:23 +0900

    4th commit

---
+++ 3rd/index.js
@@ -0,0 +1,18 @@
+const button1 = document.querySelector("#추가버튼");
+const input1 = document.querySelector("#입력값");
+const todos = document.querySelector("#할일들");
+
+button1.addEventListener("click", () => {
+  const value1 = input1.value;
+  const p1 = document.createElement("p");
+  p1.innerText = value1;
+  todos.appendChild(p1);
+  input1.value = "";
+  p1.addEventListener("click", () => {
+    if (p1.className !== "done") {
+      p1.className = "done";
+    } else {
+      p1.className = "";
+    }
+  });
+});

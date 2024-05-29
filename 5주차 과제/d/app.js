b58b63e7f45dee44f2bcacb3c70767e7f7a0cef6
tlsrose <rose.de.november@gmail.com>
2024-05-29 15:25:18 +0900

    5주차 과제

--- 5주차 과제/d/app.js
+++ 5주차 과제/d/app.js
@@ -1,3 +1,14 @@
 document.querySelector("button").addEventListener("click", (e) => {
   console.log(e);
 });
+
+document.querySelector("input").addEventListener("input", (e) => {
+  console.log(e);
+});
+
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
+
+input.addEventListener("input", (e) => {
+  h1.innerText = input.value;
+});
